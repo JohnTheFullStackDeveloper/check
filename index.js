11d9b7bf4957a916a327cb07ca17b9f1a297207d
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-10-18 23:26:07 +0530

    ser

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-    res.write("hello from backend server")
-    res.end()
-});
+const http = require("http")
+const port = process.env.port || 3000
+const server = http.createServer((res,req)=>{
+    req.write("hello from backend")
+    req.end()
+})
 server.listen(port,()=>{
-    console.log(`Server started on port ${port}`);
+    console.log("listening in "+port)
 })
