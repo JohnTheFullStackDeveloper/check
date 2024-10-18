cd905c90a2b39cbf07b05e37dc114c8049f905e9
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-10-18 23:34:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http")
 const port = process.env.port || 3000
 const server = http.createServer((res,req)=>{
-    req.write("hello from backend")
+    req.write("hello john prakash")
     req.end()
 })
 server.listen(port,()=>{
