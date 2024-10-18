68c247d96967e0033c4e59ba901f79c40281cafb
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-10-18 22:56:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+    res.write("hello from backend server")
+    res.end()
+});
+server.listen(port,()=>{
+    console.log(`Server started on port ${port}`);
+});
