d408b1ff36fb77e418717554811e73a6ef96a0f7
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-10-20 22:26:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const http = require("http").Server(app)
-const io = require("socket.io")(http,{
+const port = process.env.PORT || 3000
+const http = require('http').Server(app)
+const io = require('socket.io')(http,{
     cors:{
-        origin: "*",
+        origin:"*",
         method:"GET",
-        allowHeaders:true,
+        allowedHeaders:true,
         credentials:true
     }
-});
+})
+let now = [(Math.random()*100).toFixed(2)]
+let send = [1.00]
+console.log(now)
+let Interval = setInterval(()=>{
+    sendX()
+},100)
+function sendX() {
+    io.emit("getX", send[0].toFixed(2))
+    if(now[0] < send[0]){
+        console.log("gone")
+        io.emit("getX","gone")
+        clearInterval(Interval)
+        now[0] = (Math.random()*100).toFixed(2)
+        send[0] = 1.0
+        console.log(now)
+        setTimeout(()=>{
+            Interval = setInterval(()=>{
+                sendX()
+            },100)
+        },10000)
 
-const port = process.env.PORT || 3000
+    }
+    if(send[0]<2) send[0] = send[0] + 0.01
+    else if(send[0]<5) send[0] = send[0] + 0.09
+    else if(send[0]<10) send[0] = send[0] + 0.12
+    else if(send[0]<25) send[0] = send[0] + 0.24
+    else if(send[0]<40) send[0] = send[0] + 0.36
+    else if(send[0]<50) send[0] = send[0] + 0.48
+    else if(send[0]<100) send[0] = send[0] + 0.59
+    else send[0] = send[0] + 0.91
 
-io.on("connection", (socket) => {
-    socket.emit("onConnect",socket.id)
+}
+let all = []
+io.on('connection', (socket) => {
+    all.push(socket)
+    console.log('Connected '+socket.id)
+})
+io.on('disconnect', (socket) => {
+    let removeIndex = all.findIndex(e => e.id === socket.id)
+    if (removeIndex === -1) {
+
+    }
+    else{
+        all.splice(removeIndex, 1)
+    }
 })
 
+
+
 http.listen(port, () => {
-    console.log("Listening on port " + port)
+    console.log(`Listening on port ${port}`)
 })
