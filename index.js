a1c21da7bd527bebe899d35e1f5b838cdc635335
JohnTheFullStackDeveloper <prakashjohn7878@gmail.com>
2024-10-20 23:07:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const http = require('http').Server(app)
 const io = require('socket.io')(http,{
     cors:{
-        origin:"localhost:3001",
+        origins:"*",
         method:"GET",
         allowedHeaders:true,
         credentials:true
